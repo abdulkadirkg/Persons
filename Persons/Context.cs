cde7bd0d5207987265dbb76348ad5a1f12238174
Abdulkadir KG <abdulkadir-karadag@hotmail.com>
2023-03-21 10:16:28 +0300

    Add project files.

---
+++ Persons/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace Persons;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+        ChangeTracker.AutoDetectChangesEnabled = false;
+        
+    }
+    public DbSet<Person> Persons { get; set; }
+}
+
---
+++ Persons/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persons.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PersonController : ControllerBase
+{
+    private readonly Context _context;
+
+    public PersonController(Context context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("insert")]
+    public async Task<IActionResult> Insert()
+    {
+        await _context.AddAsync(new Person { name = "Ali", surname = "Veli" });
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpGet("read")]
+    public async Task<IActionResult> Read()
+    {
+        return Ok(await _context.Persons.ToArrayAsync());
+    }
+}
